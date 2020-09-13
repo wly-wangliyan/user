704d8f19a8ed4b4479a7797f6ef132af93d6b84b
wangliyan <wangliyan@qq.com>
2020-09-13 16:42:03 +0800

    add

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 import { DetailsComponent } from '../details/details.component';
 import { UserService } from '../user.service';
+import { AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-edit',
@@ -8,6 +9,11 @@ import { UserService } from '../user.service';
   styleUrls: ['./edit.component.less']
 })
 export class EditComponent implements OnInit {
+  @ViewChild('age') age: any;
+  @ViewChild('ageTip') ageTip: any;
+  @ViewChild('tel') tel: any;
+  @ViewChild('telTip') telTip: any;
+  public ageStatus: boolean = false;
   public obj: any = {
     name: '',
     age: '',
@@ -15,11 +21,19 @@ export class EditComponent implements OnInit {
     tel: '',
     address: ''
   };
-  public disabled = false;
+
+  private essential: any = {
+    name: '',
+    age:'',
+    sex: '',
+    tel: '',
+  };
+
   constructor(public userEdit: UserService) { }
 
   ngOnInit(): void {
     this.getUserInfo();
+    this.userEdit.isNull(this.obj);
   }
 
   setUserInfo(): void{
@@ -29,5 +43,42 @@ export class EditComponent implements OnInit {
   getUserInfo(): void{
     this.userEdit.getUser();
     this.obj = this.userEdit.obj;
+  }
+
+// 校验手机号码格式
+  isTelephone(e,value) {
+    if (e.value) {
+      if (!(/^1[34578]\d{9}$/.test(e.value))) {
+        value.removeAttribute("hidden");
+        this.obj.tel = e.value = '';
+      } else {
+        value.setAttribute("hidden", true);
+        }
+    }
+  }
+
+  // 校验年龄格式
+  isNumber(e,value) {
+    if (e.value) {
+      this.ageStatus = isNaN(e.value);
+      if (this.ageStatus == true) {
+        this.obj.age = e.value = '';
+        value.removeAttribute("hidden");
+      } else {
+        value.setAttribute("hidden", true);
+      }
+    }
+  }
+// 保存时校验必填项
+  isAll() {
+    this.isNumber(this.age.nativeElement, this.ageTip.nativeElement)
+    this.isTelephone(this.tel.nativeElement,this.telTip.nativeElement)
+    for (const key in this.essential) {
+      this.essential[key] = this.obj[key]
+      if (!this.obj[key]){
+        alert("请将必填项填写完整");
+        return;
+      }
+    }
   }
 }
